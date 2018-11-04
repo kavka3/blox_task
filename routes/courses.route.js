829f5f58965e9dcceb72d5dfcceb3d7392ffd4f9
Oleg Shmuelov <oleg.shmuelov@gmail.com>
2018-11-05 01:35:32 +0200

    Update routes/courses.route.js

--- routes/courses.route.js
+++ routes/courses.route.js
@@ -2,15 +2,23 @@ import express from "express";
 import courseController from "../controllers/courses.controller"
 const router = express.Router()
 
-router.get('/allcourses', (req, res) => {
+router.get('/', (req, res) => {
     courseController.getAll(req, res);
 });
 
-router.post('/addcourse', (req, res) => {
+router.get('/:id', (req, res) => {
+    courseController.getById(req, res);
+});
+
+router.post('/', (req, res) => {
     courseController.addCourse(req, res);
 });
 
-router.delete('/deletecourse', (req, res) => {
+router.put('/:id', (req, res) => {
+    courseController.updateCourse(req, res);
+});
+
+router.delete('/:id', (req, res) => {
     courseController.deleteCourse(req, res);
 });
 
